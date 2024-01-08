721c14cc0e3955fd9643ec353324eac2ca028758
Alan Rostem <42648988+AlanRostem@users.noreply.github.com>
2024-01-08 14:34:51 +0100

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Auto alarm
+
+Simple alarm python script that plays a music made by my friend when break/work time is over. 
+
+Press enter to dismiss the break/work alarm. The script automatically alternates between break/work time.
+
+Great for people like myself who hate keeping track of time :)
+
+## Python packages
+
+- keyboard
+- pygame
---
+++ main.py
@@ -0,0 +1,45 @@
+import pygame
+import keyboard
+import time 
+import sys
+from config import *
+
+if __name__ == "__main__":
+    pygame.mixer.init()
+    pygame.mixer.music.load("turbo_trooper_theme.wav")
+
+    print("Controls:")
+    print("\t<esc>: Quit program")
+    print("\t<enter>: Dismiss alarm")
+
+    is_working = True
+    is_alarming = False
+
+    while True:
+        if keyboard.is_pressed("esc"):
+            break
+
+        if is_alarming:
+            if keyboard.is_pressed("enter"):
+                pygame.mixer.music.stop()
+                is_alarming = False
+                is_working = not is_working
+                if is_working:
+                    sys.stdout.flush()
+                    sys.stdout.write(f"\rWorking {WORK_TIME_MIN} mins.                ")
+                else:
+                    sys.stdout.flush()
+                    sys.stdout.write(f"\rOn break for {BREAK_TIME_MIN} mins.                ")
+            continue
+        
+        if is_working:
+            time.sleep(WORK_TIME_MIN * MIN)
+            is_alarming = True
+            pygame.mixer.music.play()
+        else:
+            time.sleep(BREAK_TIME_MIN * MIN)
+            is_alarming = True
+            pygame.mixer.music.play()
+        
+        sys.stdout.flush()
+        sys.stdout.write(f"\rALARM!!!                      ")
